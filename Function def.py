74b389db5551d12d344bd3b45b2864e8bee678d2
migandi02 <migandi.it@hotmail.com>
2023-08-23 10:31:57 -0300

    Definiendo funciones

---
+++ Function def.py
@@ -0,0 +1,25 @@
+#-------------------------------------------------------------------------------
+# Name:        módulo1
+# Purpose:
+#
+# Author:      Migue
+#
+# Created:     23/08/2023
+# Copyright:   (c) Migue 2023
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    pass
+
+# Definiendo funciones
+# primeros pasos
+def suma(n1,n2):
+    print(n1+n2)
+
+suma (10, 20)
+
+suma (25, 50)
+
+suma (108, 30)
+
